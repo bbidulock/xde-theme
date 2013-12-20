e8114254e39ef402d615e6c74000882375461218
Brian Bidulock <bidulock@openss7.org>
2013-12-20 00:20:19 -0700

    added version control

---
+++ autogen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION=
+
+if [ -z "$VERSION" ]; then
+	VERSION='1.1'
+	if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
+		VERSION=$(git describe)
+	fi
+fi
+
+sed -i -e "s:[[]xde-styles[]],[[][^]]*[]]:[xde-styles],[$VERSION]:
+	   s:AC_REVISION([[][^]]*[]]):AC_REVISION([$VERSION]):" configure.ac
+
+autoreconf -fiv
